e063edd54e901094d5958f13b33704917a8a1620
ladanov <ladanov_n@mail.ru>
2018-06-18 22:06:35 +0300

    toogle menu

--- src/components/fullScreenMenu.vue
+++ src/components/fullScreenMenu.vue
@@ -1,7 +1,13 @@
 <template>
     <div id="full_screen_menu" class="full_screen_menu">
       <canvas id="full_screen_menu-canvas_video" class="canvas_video"></canvas>
-      <div v-bind:class="{'open': $store.state.visible_blackout, 'full_screen_menu-grid': true}">
+      <div v-on:click="toggle_menu" class="toggle_menu">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div v-bind:class="{'active': $store.state.visible_full_screen_menu, 'full_screen_menu-grid': true}">
           <div class="grid-item">
             <img v-on:click="ch_volume_of_music" src="/static/img/fullscreenMenu/equalizer.gif">
           </div>
@@ -62,6 +68,18 @@ export default {
       if (audio) {
         audio.volume = this.$store.state.volume_of_music;
       }
+    },
+    toggle_menu: function(e) {
+      console.log(e.currentTarget.className);
+      if (e.currentTarget.className == "toggle_menu") {
+        e.currentTarget.className = "toggle_menu open";
+        this.$store.commit("ch_visible_blackout", true);
+        this.$store.commit("ch_visible_full_screen_menu", true);
+      } else {
+        e.currentTarget.className = "toggle_menu";
+        this.$store.commit("ch_visible_blackout", false);
+        this.$store.commit("ch_visible_full_screen_menu", false);
+      }
     }
   }
 };
@@ -89,6 +107,84 @@ export default {
   grid-template-rows: 33% 34% 33%;
   justify-items: center;
   align-items: center;
+  transition: 0.3s;
+  opacity: 0;
+}
+
+.full_screen_menu-grid.active {
+  opacity: 1;
+}
+
+.toggle_menu {
+  position: relative;
+  cursor: pointer;
+  z-index: 1000;
+  width: 30px;
+  height: 20px;
+  margin-left: auto;
+  top: 30px;
+  right: 30px;
+}
+
+.toggle_menu span {
+  display: block;
+  position: absolute;
+  height: 2px;
+  width: 100%;
+  background: #aaa;
+  border-radius: 1px;
+  opacity: 1;
+  left: 0;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: 0.25s ease-in-out;
+  -moz-transition: 0.25s ease-in-out;
+  -o-transition: 0.25s ease-in-out;
+  transition: 0.25s ease-in-out;
+}
+
+.toggle_menu span:nth-child(1) {
+  top: 0px;
+}
+
+.toggle_menu span:nth-child(2) {
+  top: 8px;
+}
+
+.toggle_menu span:nth-child(3) {
+  top: 8px;
+}
+
+.toggle_menu span:nth-child(4) {
+  top: 16px;
+}
+
+.toggle_menu.open span:nth-child(1) {
+  top: 8px;
+  width: 0%;
+  left: 50%;
+}
+
+.toggle_menu.open span:nth-child(2) {
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  -o-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+}
+
+.toggle_menu.open span:nth-child(3) {
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+.toggle_menu.open span:nth-child(4) {
+  top: 8px;
+  width: 0%;
+  left: 50%;
 }
 
 .full_screen_menu-grid .grid-item {
